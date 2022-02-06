4984b5b48ec6f1560a434e9a26ae61ae3604e5c2
RandyRDavila <davilar@uhd.edu>
2022-02-06 17:50:33 -0600

    Initial commit and push to Github.

---
+++ Graphs_and_JuMP_Episode_6/maximimum_independent_set.jl
@@ -0,0 +1,30 @@
+using JuMP
+using GLPK
+using Graphs
+using GraphPlot
+using GraphIO
+
+
+function maximum_independent_set(graph::SimpleGraph{Int64}; show_model = false)
+    model = Model(GLPK.Optimizer)
+    @variable(model, x[1:Graphs.nv(graph)], Bin)
+    @objective(model, Max, sum(x))
+    for (i, e) in enumerate(Graphs.edges(graph))
+        @constraint(model, x[Graphs.src(e)] + x[Graphs.dst(e)] <= 1)
+    end
+    optimize!(model)
+    if show_model
+        println(model)
+    end
+    return [xi for xi in x if value(xi) == 1.0]
+end
+
+# Read in the Petersen graph edge list text file 
+graph = Graphs.loadgraph("PetersenGraph.txt", "graph_key", GraphIO.EdgeListFormat())
+# Convert the directed graph to a simple graph
+graph = Graphs.SimpleGraph(graph)
+
+# Solve the maximimum independent set problem on the Petersen graph
+max_independent_set = maximum_independent_set(graph, show_model = true)
+
+println(max_independent_set)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# TalkJulia_with_Randy
+# TalkJulia with Randy
+
+This repository contains Julia code written and discussed by Randy Davila during the TalkJulia podcast with David Amos. Over the coming year I plan on updating this repository with many different examples of Julia code. 
+
